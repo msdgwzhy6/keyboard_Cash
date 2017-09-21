6b1ba25f8cb8617472906a5ee9a8614153149f43
qzs <qzs531156@163.com>
2017-09-21 15:15:31 +0800

    update更新键盘界面

--- app/src/main/java/com/example/qzs/keyboard_cash/PayKeyAdapter.java
+++ app/src/main/java/com/example/qzs/keyboard_cash/PayKeyAdapter.java
@@ -1,7 +1,6 @@
 package com.example.qzs.keyboard_cash;
 
 import android.content.Context;
-import android.graphics.Color;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -13,101 +12,115 @@ import android.widget.TextView;
 public class PayKeyAdapter extends BaseAdapter {
 	private Context mContext;
 	
-	private Context context;  
+	private Context context;
 	   
-	 public PayKeyAdapter(Context context) {  
+	 public PayKeyAdapter(Context context) {
 		 this.context=context;  
 	 }  
 	   
-	 private String[] texts = {  
+	 private String[] texts = {
 	   //九宫格图片下方文字的设置  
 	   "1",  
 	   "2",  
 	   "3",  
-	   "清空",
+	 //  "清空",
 	   "4",  
 	   "5",  
 	   "6",  
-	   "←",
+	 //  "←",
 	   "7",  
 	   "8",  
 	   "9",  
-	   "=",
+	 //  "=",
 	   ".",  
 	   "0",
-	   "+",
-	   "收款",
+			 "✖"
+	  // "+",
+	 //  "收款",
 	   }; 
 	 
-	 private String[] types = {  
+	 private String[] types = {
 	   //存放在text的tag中
 	   "num",  
 	   "num",  
 	   "num",  
-	   "cancel",
+	//   "cancel",
 	   "num",  
 	   "num",  
 	   "num",  
-	   "back",
+	//   "back",
 	   "num",  
 	   "num",  
 	   "num",  
-	   "equal",
+	 //  "equal",
 	   "point",  
 	   "num",
-	   "plus",
-	   "pay",
+			 "back",
+	 //  "plus",
+	 //  "pay",
 	 };
 	 
-	 @Override  
+	 @Override
 	 public int getCount() {  
 	  return texts.length;  
 	 }  
 	 
-	 @Override  
-	 public Object getItem(int position) {  
+	 @Override
+	 public Object getItem(int position) {
 	  return position;  
 	 }  
 	 
-	 @Override  
+	 @Override
 	 public long getItemId(int position) {  
 	  return position;  
 	 }  
 	 
-	 @Override  
-	 public View getView(int position, View view, ViewGroup viewgroup) {  
+	 @Override
+	 public View getView(int position, View view, ViewGroup viewgroup) {
 	  ImgTextWrapper wrapper;  
 	  if(view==null) {  
 	   wrapper = new ImgTextWrapper();  
-	   LayoutInflater inflater = LayoutInflater.from(context);  
+	   LayoutInflater inflater = LayoutInflater.from(context);
 	   view = inflater.inflate(R.layout.grid_menu_item_pay, null);
 	   view.setTag(wrapper);  
-	   view.setPadding(3, 3, 3, 3);  //每格的间距  
+	   view.setPadding(26, 26, 26, 26);  //每格的间距
 	  } else {  
 	   wrapper = (ImgTextWrapper)view.getTag();  
 	  }  
 	    
-	  wrapper.textView = (TextView)view.findViewById(R.id.GridTextView);
-	  wrapper.textView.setText(texts[position]);  
+	     wrapper.textView = (TextView)view.findViewById(R.id.GridTextView);
+		 wrapper.iv_back= (ImageView) view.findViewById(R.id.iv_back);
+
+		  if (position<11){
+			 wrapper.textView.setVisibility(View.VISIBLE);
+			 wrapper.iv_back.setVisibility(View.GONE);
+			 wrapper.textView.setText(texts[position]);
+		 }else {
+			 wrapper.textView.setVisibility(View.GONE);
+			 wrapper.iv_back.setVisibility(View.VISIBLE);
+		 }
+
 	  wrapper.textView.setTag(types[position]);
 	  if(types[position].equals("back")
 			  || types[position].equals("cancel")
 			  || types[position].equals("equal")){
-		  wrapper.textView.setBackgroundResource(R.drawable.btn_cell_delete);
+		 // wrapper.textView.setBackgroundResource(R.drawable.btn_cell_delete);
 	  }else if(types[position].equals("pay")){
-		  wrapper.textView.setBackgroundResource(R.drawable.btn_cell_pay);
-		  wrapper.textView.setTextColor(Color.WHITE);
+		//  wrapper.textView.setBackgroundResource(R.drawable.btn_cell_pay);
+		//  wrapper.textView.setTextColor(Color.WHITE);
 	  }
 			  
 //	  wrapper.textView = (TextView)view.findViewById(R.id.GridItemText);  
 //	  wrapper.textView.setText(texts[position]);
 //	  wrapper.textView.setTag(payTypes[position]);
-	    
+
+
+
 	  return view;  
 	 }  
 	 
 	 class ImgTextWrapper {  
-		 ImageView imageView;  
-		 TextView textView;  
+		 ImageView iv_back;
+		 TextView textView;
 	 } 
 }
